ad102985ed76757e52288a64f83524f67e7f7f86
AmbarRenata <ambarrenata5@gmail.com>
2023-10-02 10:13:58 -0700

    hi

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include "main.h"
-
 /**
- * print_alphabet - print all alphabet in lowercase
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-void print_alphabet(void)
+int main(void)
 {
-	char k;
-
-	for (k = 'a' ; k <= 'z' ; k++)
-		_putchar(k);
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
